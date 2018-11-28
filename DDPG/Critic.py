2735993b58776a266fb0b206b6bbb41f34c37cff
Theo <theo.gruner@gmail.com>
2018-11-28 10:54:50 +0100

    neu

--- DDPG/Critic.py
+++ DDPG/Critic.py
@@ -20,3 +20,5 @@ class DQN(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
+
+#hier könnte ihre Werbung stehen
